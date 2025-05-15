0e3e3c836fca64221e87e546325735fdeaa96a63
Rishav <rishav.raj@sellergeni.com>
2025-05-15 21:53:41 +0530

    leetcode-1

---
+++ 217.py
@@ -0,0 +1,32 @@
+
+# 217. Contains Duplicate
+# https://leetcode.com/problems/contains-duplicate/
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        my_set= set()
+        for num in nums:
+            if num in my_set:
+                return True
+            my_set.add(num)
+        return False   
+
+# Dict
+        # my_dict={}
+        # for num in nums:
+        #     # my_dict[num] = False
+        #     if num in my_dict:
+        #         my_dict[num] = True
+        #         return True
+        #     else:
+        #         my_dict[num]= False
+                
+        # # print(my_dict)
+        # # for x in my_dict.values():
+        # #     print(x)
+        # #     if x == True:
+        # #         return True
+        # return False   
